34f7eec942fa23b8ed59a33413ebdfc1ffd0e74e
talkdsign <talkdsign@gmail.com>
2020-01-05 01:46:38 -0300

    criar express

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const routes = require('./routes');
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', routes);
+
+app.listen(3000, err =>{
+    if(err){
+        console.log('Não foi possível conectar ao servidor.')
+    }else{
+        console.log('Servidor conectado')
+    }
+})
