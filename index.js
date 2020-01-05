07c6ec98ae2a8e6cf368948bebb5bb61f9a2b368
talkdsign <talkdsign@gmail.com>
2020-01-05 03:25:45 -0300

    criação cotacao, convert, resultado

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path')
 const routes = require('./routes');
 
 
+
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
--- routes.js
+++ routes.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
+const convert = require('./lib/convert')
+
 //Home
 router.get('/', (req, res) =>{
     res.render('home')
+})
+
+router.get('/cotacao', (req, res) =>{
+    const { cotacao, quantidade } = req.query
+    if(cotacao && quantidade){
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', { 
+            error: false,
+            conversao: convert.toMoney(conversao),
+            quantidade: convert.toMoney(quantidade),
+            cotacao: convert.toMoney(cotacao)
+    
+        
+        })
+    }else{
+        res.render('cotacao', {
+            error: 'Valores inválidos'
+        })
+    }
+    
 })
 
 module.exports = router;
