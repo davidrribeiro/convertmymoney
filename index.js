8d9e6128394d1babe28f379b7548f50b7ef675e7
talkdsign <talkdsign@gmail.com>
2020-01-06 03:44:43 -0300

    add body-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const convert = require('./lib/convert')
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //Home
 app.get('/', (req, res) =>{
